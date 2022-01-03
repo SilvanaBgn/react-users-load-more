86d2765c8d790f84b45b803c60d35fb33fa42d2b
silvana <silvana.belen.garcia@gmail.com>
2022-01-03 18:19:06 -0300

    Users Get to https://randomuser.me

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Header from './components/Header';
+import axios from 'axios';
 
 export default class App extends React.Component {
+  state = {
+    users: [],
+    isLoading: false,
+    errorMsg: ''
+  };
+
+  componentDidMount() {
+    console.log('1');
+    this.setState({ isLoading: true });
+
+    axios
+      .get('https://randomuser.me/api/?page=0&results=10')
+      .then((response) => {
+        console.log('2');
+        this.setState({ users: response.data.results, errorMsg: '' });
+      })
+      .catch(() =>
+        this.setState({
+          errorMsg: 'Error while loading data. Try again later.'
+        })
+      )
+      .finally(() => {
+        console.log('3');
+        this.setState({ isLoading: false });
+      });
+
+    /*fetch('https://randomuser.me/api/?page=0&results=10')
+      .then((response) => {
+        console.log('Axios users:', response.json());
+      })
+      .catch((error) => console.log('Error:', error)); */
+  }
+
   render() {
+    console.log('0');
+    const { users, isLoading, errorMsg } = this.state;
+    console.log('Rendering:', users);
+
     return (
       <div className="main-section">
         <Header />
-        <h2>App Component</h2>
+        {isLoading && <p className="loading">Loading...</p>}
+        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
       </div>
     );
   }
